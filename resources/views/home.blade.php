b48c002eff833dc53ee76297ce6f55fef7c6716c
MirkOrlando <mirko@orlando-m.it>
2022-06-13 15:20:07 +0200

    add movies data

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="movies">
+        <div class="container">
+            <h1>My Movies</h1>
+            <div class="row row-cols-2 row-cols-md-3 row-cols-xl-5 gy-3">
+                @forelse ($movies as $movie)
+                    <div class="col">
+                        <div class="card h-100">
+                            <div class="card-body">
+                                <h4 class="card-title">{{ $movie->title }}</h4>
+                                <p class="card-text"><strong>Original title:</strong>{{ $movie->original_title }}</p>
+                            </div>
+                        </div>
+                    </div>
+                @empty
+                    <p>Nothing to show</p>
+                @endforelse
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/homepage.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    site main here
-@endsection
