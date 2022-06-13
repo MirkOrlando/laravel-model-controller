6fa88a09db2ae444838e93ce902e0f9bd01d6186
MirkOrlando <mirko@orlando-m.it>
2022-06-13 16:49:16 +0200

    add dynamic class 'active'

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,8 +2,10 @@
     <div class="container">
         <nav class="navbar navbar-expand navbar-light">
             <div class="nav navbar-nav">
-                <a class="nav-item nav-link active" href="{{ route('home') }}">Home</a>
-                <a class="nav-item nav-link" href="{{ route('movies.index') }}">Movies</a>
+                <a class="nav-item nav-link text-uppercase {{ Route::currentRouteName() === 'home' ? 'active' : '' }}"
+                    href="{{ route('home') }}">Home</a>
+                <a class="nav-item nav-link text-uppercase {{ Route::currentRouteName() === 'movies.index' ? 'active' : '' }}"
+                    href="{{ route('movies.index') }}">Movies</a>
             </div>
         </nav>
     </div>
